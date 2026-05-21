935a8802ef4f48f92ce3b754ce78a8fe12c3b17b
Professor <rafael.zottesso@ifpr.edu.br>
2026-05-21 09:19:39 -0300

    Preparação do projeto para o Google Cloud e Deploy

--- pw2026/settings.py
+++ pw2026/settings.py
@@ -141,12 +141,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+STATIC_ROOT = 'static_gcloud/'  # Alterar essa configuração
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static',
 ]
-# https://dontpad.com/zottesso/pw
 
+# https://dontpad.com/zottesso/pw
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
